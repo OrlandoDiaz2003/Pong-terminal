fa41c98a1fb8472e1c22866bb385f475c63f286a
Orlando <or.diaz@duocuc.cl>
2025-07-05 18:55:15 -0400

    paddle collision with ball

--- test.c
+++ test.c
@@ -5,31 +5,41 @@
 
 static void finish(int sig);
 
-void ball( int * ball_x, int *ball_y, int *ball_vel_x, int *ball_vel_y ){
+void ball( float * ball_x, float *ball_y, float *ball_vel_x, float *ball_vel_y, float *paddle_height,float *paddle_x, float * paddle_y){
 
     //deleting the last draw of the ball
     mvaddch(* ball_y, * ball_x, ' ');
 
     //ball speed 
-    * ball_x += * ball_vel_x;
+    float acomulator = acomulator + *ball_vel_x;
+
+    * ball_x += acomulator;
     * ball_y += * ball_vel_y;
 
     //collision with borders 
     if(* ball_x <= 0 || *ball_x >= COLS - 1){
-      *  ball_vel_x *= - 1 ; 
+      * ball_vel_x *= - 1 ; 
+      * ball_x = (COLS/2) - 1;
+      * ball_y = LINES/2;
     }
+
     if(* ball_y <= 0 || * ball_y >= LINES - 1){
       *  ball_vel_y *= - 1 ; 
     }
 
+    //collision with paddle
+     
+    if(* ball_x <= * paddle_x && * paddle_y <= * ball_y && * paddle_y + * paddle_height >= * ball_y){
+        * ball_vel_x *= -1;
+    }
+
     //drawing ball in the new position
     mvaddch(* ball_y, * ball_x, 'O');
-
 }
 
-void paddle(int * paddle_height, int * paddle_x,int * paddle_y){
-    
 
+void paddle(float * paddle_height,float  * paddle_x,float * paddle_y){
+    
     // getting user input
     int key = getch();
 
@@ -39,22 +49,19 @@ void paddle(int * paddle_height, int * paddle_x,int * paddle_y){
         mvaddch(*paddle_y + i, *paddle_x, ' ');
     }
 
-    if(key == KEY_UP && * paddle_height > 0) (* paddle_y) --;
-    
+    //move the paddle
+    if(key == KEY_UP && * paddle_y > 0) (* paddle_y) --;
     if(key == KEY_DOWN && * paddle_y + * paddle_height < LINES) (* paddle_y) ++;
 
     //Draw paddle
     for (int i = 0; i < *paddle_height; i++)
     {
-        mvaddch(*paddle_y + i, *paddle_x, '#');
+        mvaddch(*paddle_y + i, *paddle_x,'#');
     }
-
-
 }
 
 int main()
 {
-    //inicia screen
     initscr();
 
     nodelay(stdscr, TRUE);
@@ -66,25 +73,24 @@ int main()
 
     //BALL VARIABLES
     //ball position
-    int ball_y = 0;
-    int ball_x = COLS / 2;
+    float ball_y = 0;
+    float ball_x = COLS / 8;
 
-    //ball velocity
-    int ball_vel_x = 1;
-    int ball_vel_y = 1;
-
+    // ball velocity
+    float ball_vel_x = 0.5;
+    float ball_vel_y = 0.2;
 
     //PADDLE VARIABLES
     //paddle height 
-    int paddle_height = 4;
+    float paddle_height = 4;
 
     //paddle position
-    int paddle_x = 0;
-    int paddle_y = 0;
+    float paddle_x = (COLS / 2) - (COLS / 2) + 10;
+    float paddle_y = 0;
 
     for(;;)
     {
-        ball(&ball_x, &ball_y, &ball_vel_x, &ball_vel_y);
+        ball(&ball_x, &ball_y, &ball_vel_x, &ball_vel_y,&paddle_height,&paddle_x, &paddle_y);
         paddle( &paddle_height, &paddle_x, &paddle_y);
 
         refresh();
